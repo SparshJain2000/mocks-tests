1cecd930d05a518c3e2aee1d0abc95ea46aca2e4
SparshJain2000 <jainsparsh0801@gmail.com>
2021-07-09 17:14:36 +0530

    Add problem statements

--- amazon/mock1/amazon_delivery.cpp
+++ amazon/mock1/amazon_delivery.cpp
@@ -1,3 +1,33 @@
+/*
+Amazon Fresh is a grocery delivery service that offers consumers the option of purchasing their groceries online and having them delivered on schedule. The Amazon Fresh team is planning a route for a delivery truck to deliver customer orders in the city of Techlandia. The planner will create a delivery area for each order to effectively plan the route. The area is abstracted as a grid. Not all locations are accessible by road. The truck only needs to make a single delivery
+Write an algorithm to determine the minimum distance required for the truck to deliver the order.
+
+ASSUMPTIONS:
+
+Some places in the delivery area cannot be accessed by the deliver, as there are no roads in those locations.
+The delivery area can be represented as a two-dimensional grid of integers, where each integer represents one cell.
+The truck must start from the top left. corner of the area, which is always accessible and can move one cell up, down, left, or right at a time. The truck must navigate around the areas without roads and cannot leave the area.
+- The accessible areas are represented as 1 areas without roads are represented by 0 and the order destination is represented by 9.
+
+* Input format
+The first line of input denotes the matrix size. The following lines denote the value of each row
+
+* Output format
+Return an integer representing the total distance traversed to deliver the order else return-1
+
+* Code constraints
+1 <= rows, columns <= 10^3
+
+* Sample testcases
+Input 1
+3
+1 0 0
+1 0 0
+1 9 1
+Output 1
+3
+
+*/
 #include <bits/stdc++.h>
 using namespace std;
 int util(vector<vector<int> > &grid, int i, int j, vector<vector<bool> > &vis) {
--- amazon/mock1/amazon_encoder.cpp
+++ amazon/mock1/amazon_encoder.cpp
@@ -1,3 +1,30 @@
+/*
+Amazon Media Encoder is a highly scalable, easy to use and cost-effective way for developers and businesses to convert media files from their source format Into versions that will playback on devices like smartphones. Whenever a media file needs to be processed by the encoder it partitions the media file into N sub files. Each sub-file is processed independently. During this process each sub file gets compressed These compressed sub-files are their merged by the encoder.
+
+The encoder can merge only two files at a time. The time required to merge the two file is equal to the sum of their sizes. The size of this merged file is also equal to the sum of their sizes. The process is repeated until the N sub files are merged into a single output file.
+
+Write an algorithm to output the minimum possible time to merge the given N sub files into a single file.
+
+* Input format
+The input to the function/mentioned consists of one argument- file sizes a list of integers representing the size of the compressed sub files
+
+* Output format
+Return an integer representing the minimum time required to merge all the sub-file
+
+* Code constraints
+
+1 <= number of sub-files <= 10^6
+1 <= file sizes[i] <= 10^6 
+0 <= i < number of sub files
+
+* Sample testcases
+Input 1
+4
+4 8 6 12
+Output 1
+58
+
+*/
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
